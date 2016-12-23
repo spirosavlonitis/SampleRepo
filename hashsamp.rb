73b2941fb4e743545f6d9382ab318973b74cbecc
spirosavlonitis <spirosa84@hotmail.com>
2016-12-23 04:05:56 +0200

    Update hashsamp.rb

--- hashsamp.rb
+++ hashsamp.rb
@@ -3,3 +3,9 @@ samphash = {nameA:{first_name:"some",last_name:"one"},nameB:"Some One"}
 puts samphash[:nameA][:first_name]
 
 puts samphash[:nameB]
+
+
+samp_array = [[1,2],[3,4]]
+
+
+puts samp_array[0][1]
